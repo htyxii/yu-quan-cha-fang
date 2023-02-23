dbd2600226579ba7b89f8173f2985244fc2fa606
htyxii <hty.xii@gmail.com>
2023-02-23 21:51:26 +0800

    Feat: Add Slider Component. Code Refactoring

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,11 +8,14 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { IndexComponent } from './index/index.component';
 import { ToolbarComponent } from './toolbar/toolbar.component';
 
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatIconModule } from '@angular/material/icon';
+// Angular Material
 import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
+// ng-image-slider
+import { NgImageSliderModule } from 'ng-image-slider';
 
 @NgModule({
   declarations: [
@@ -25,12 +28,11 @@ import { MatMenuModule } from '@angular/material/menu';
     AppRoutingModule,
     BrowserAnimationsModule,
     MatToolbarModule,
-    MatIconModule,
     MatButtonModule,
+    MatIconModule,
+    MatMenuModule,
     MatToolbarModule,
-    MatIconModule,
-    MatButtonModule,
-    MatMenuModule
+    NgImageSliderModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/index/index.component.ts
+++ src/app/index/index.component.ts
@@ -7,4 +7,19 @@ import { Component } from '@angular/core';
 })
 export class IndexComponent {
 
+  imageObject: Array<object> = [
+    {
+      image: './assets/images/banner_1.jpg',
+      thumbImage: './assets/images/banner_1.jpg',
+      title: '100% 台灣茶葉',
+      alt: 'alt of image',
+    }, 
+    {
+      image: './assets/images/banner_2.jpg', // Support base64 image
+      thumbImage: './assets/images/banner_2.jpg', // Support base64 image
+      title: '絕無混充任何進口茶', //Optional: You can use this key if want to show image with title
+      alt: 'Image alt', //Optional: You can use this key if want to show image with alt
+    }
+  ];
+
 }
